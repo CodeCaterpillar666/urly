718570554ede4cc33de0ac85a83a22e83b4771ea
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-03-24 20:24:04 -0700

    Add starter code

---
+++ src/test/java/com/codecaterpillar/urly/UrlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecaterpillar.urly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
